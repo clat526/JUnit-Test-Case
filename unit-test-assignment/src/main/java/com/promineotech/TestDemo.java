da59805a179cc744ea7e1daf68a00c9f7dc99bf9
Gitman <caleblatnie7@gmail.com>
2025-05-13 11:03:06 -0700

    Week 12 Coding Assignment

---
+++ unit-test-assignment/src/main/java/com/promineotech/TestDemo.java
@@ -0,0 +1,35 @@
+package com.promineotech;
+
+import java.util.Random;
+
+public class TestDemo {
+	public int addPositive(int a, int b) {
+		if(a > 0 && b > 0) {
+			return a + b;
+		} else {
+			throw new IllegalArgumentException("Both parameters must be positive");
+		}
+	
+	}
+   int randomNumberSquared() {
+	   int squared = getRandomInt();
+	   return squared * squared;
+   }
+	
+	
+	int getRandomInt() {
+
+	    Random random = new Random();
+
+	    return random.nextInt(10) + 1;
+
+   
+	}
+ public int addNegative(int a, int b) {
+	 if(a < 0 && b < 0) {
+		 return a + b;
+	 } else {
+		 throw new IllegalArgumentException("Both parameters must be negative");
+	 }
+ }
+}
